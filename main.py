8721db10c44beb41bb7977167484c7486f9e3180
Elmer Dema <elmerdema2022@gmail.com>
2023-05-20 18:30:20 +0200

    GUI completed

--- main.py
+++ main.py
@@ -46,14 +46,76 @@ comparison to the input image) is done in a thread.
 thread again.
 '''
 import tkinter as tk
+from tkinter import Text
 
 
-class Editor:
-    def __init__(self,path):
+class Editor():
+    def __init__(self, path, root):
         self.path = path
+        self.root = root
 
     def header(self):
-        tk.Label(self.root, text="ASCII Art Editor", font=("Arial", 20)).grid(row=0, column=0, columnspan=2, sticky="nsew")
+        label=tk.Label(self.root, text="ASCII Art Editor", font=("Times New Roman", 15))
+        label.place(x=10,y=10)
+
+        path_label=tk.Entry(self.root, text="Path")
+        path_label.place(x=200,y=15)
+
+        select_label=tk.Button(self.root,text="Select Image", bg="lightgray")
+        select_label.place(x=350,y=10)
+
+        save_label=tk.Button(self.root,text="Save File", bg="lightgray")
+        save_label.place(x=440,y=10)
+
+    def body(self):
+        text_field=Text(self.root, bg="lightgray",width=60, height=20)
+        
+        text_field.place(x=10,y=50)
+
+    def footer(self):
+        font_size_entry=tk.Entry(self.root)
+        font_size_entry.place(x=10,y=400,width=25)
+
+        char_width_entry=tk.Entry(self.root)
+        char_width_entry.place(x=60,y=400,width=25)
+
+        char_height_entry=tk.Entry(self.root)
+        char_height_entry.place(x=110,y=400,width=25)
+
+        char_height_entry=tk.Entry(self.root)
+        char_height_entry.place(x=110,y=400,width=25)
+
+        characters_entry=tk.Entry(self.root)
+        characters_entry.place(x=160,y=400,width=75)
+
+        generate_button=tk.Button(self.root,text="Generate", bg="lightgray")
+        generate_button.place(x=440,y=395)
+
+        font_size_label=tk.Label(self.root,text="Size", font=("Times New Roman", 10))
+        font_size_label.place(x=8,y=420)
+
+        font_width_label=tk.Label(self.root,text="Width", font=("Times New Roman", 10))
+        font_width_label.place(x=56,y=420)
+
+        font_height_label=tk.Label(self.root,text="Height", font=("Times New Roman", 10))
+        font_height_label.place(x=106,y=420)
+
+        characters_label=tk.Label(self.root,text="Characters", font=("Times New Roman", 10))
+        characters_label.place(x=160,y=420)
 
 
 
+
+
+
+
+root = tk.Tk()
+root.title("ASCII Art Editor")
+root.geometry("500x450")
+test = Editor("test", root)
+
+test.header()
+test.body()
+test.footer()
+
+root.mainloop()
