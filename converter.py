d15aa647fab6776b7316b1a3e26d3c806fd472ca
Elmer Dema <elmerdema2022@gmail.com>
2023-05-29 15:18:09 +0200

    converter added to main

--- converter.py
+++ converter.py
@@ -5,43 +5,52 @@ https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into
 https://pythoncircle.com/post/674/python-script-13-generating-ascii-code-from-image/
 
 '''
-strings = ".:-=+*#%@"
-font_width = 20
-font_height = 30
-background_color = 255
-font_size = 10
-foreground = 0  
-
 class Ascii:
-    def __init__(self, path, strings):
+    def __init__(self, path, strings, font_width, font_height, background_color, font_size, foreground,type):
         self.path = path
-        
         self.strings = strings
+        self.font_width = font_width
+        self.font_height = font_height
+        self.background_color = background_color
+        self.font_size = font_size
+        self.foreground = foreground
+        self.type=type
 
     def grayscale(self):
         img = Image.open(self.path)
+        if self.type == 'R':
+            img = img.split()[0]
+            img.save('grayscale.jpg')
+        elif self.type == "G":
+            img = img.split()[1]
+            img.save('grayscale.jpg')
+        else:
+            img = img.split()[2]
+            img.save('grayscale.jpg')
+
         img = img.convert('L')
         img.save('grayscale.jpg')
         return img
 
+
     def create_fontimage(self):
         for i in self.strings:
-            img=Image.new('L', (font_width, font_height), color=background_color)
-            font = ImageFont.truetype('arial.ttf', font_size)
+            img = Image.new('L', (self.font_width, self.font_height), color=self.background_color)
+            font = ImageFont.truetype('arial.ttf', self.font_size)
             draw = ImageDraw.Draw(img)
-            draw.text((font_width//2, font_height//2), i, fill=foreground, font=font)
+            draw.text((self.font_width//2, self.font_height//2), i, fill=self.foreground, font=font)
             img.save(str(self.strings.index(i)) + ".jpg")
 
     def create_ascii_simple(self):
-        ascii_file=open('ascii.txt', 'w')
+        ascii_file = open('ascii.txt', 'w')
 
         # Load the grayscale image
         grayscale_img = Image.open('grayscale.jpg')
         width, height = grayscale_img.size
 
         # Get the width and height of each ASCII character
-        char_width = font_width
-        char_height = font_height
+        char_width = self.font_width
+        char_height = self.font_height
 
         # Convert each pixel of the grayscale image to ASCII characters
         ascii_repr = []
@@ -51,8 +60,8 @@ class Ascii:
                 pixel_intensity = grayscale_img.getpixel((x, y))
                 min_distance = float('inf')
                 min_char = ''
-                for char in strings:
-                    char_intensity = strings.index(char) * (255 / (len(strings) - 1))
+                for char in self.strings:
+                    char_intensity = self.strings.index(char) * (255 / (len(self.strings) - 1))
                     distance = abs(char_intensity - pixel_intensity)
                     if distance < min_distance:
                         min_distance = distance
@@ -63,13 +72,14 @@ class Ascii:
         # Print or further process the ASCII representation
         for row in ascii_repr:
             ascii_file.write(''.join(row) + '\n')
-        
+        ascii_file.close()
+
     def create_ascii_euclidean(self):
         img_euclidean = Image.open('grayscale.jpg')
         ascii_file_euclidean = open('ascii_euclidean.txt', 'w')
         width, height = img_euclidean.size
-        font_width = width // width  # Define font_width based on desired character width
-        font_height = height // height  # Define font_height based on desired character height
+        font_width = 1  # Define font_width based on desired character width
+        font_height = 1  # Define font_height based on desired character height
         ascii_euclidean = []
         for y in range(0, height, font_height):
             row = []
@@ -77,30 +87,28 @@ class Ascii:
                 pixel_intensity = img_euclidean.getpixel((x, y))
                 min_distance = float('inf')
                 min_char = ''
-                for char in strings:
-                    char_intensity = strings.index(char) * (255 / (len(strings) - 1))
-                    #euclidean distance
+                for char in self.strings:
+                    char_intensity = self.strings.index(char) * (255 / (len(self.strings) - 1))
+                    # euclidean distance
                     distance = np.linalg.norm(pixel_intensity - char_intensity)
                     if distance < min_distance:
                         min_distance = distance
                         min_char = char
                 row.append(min_char)
             ascii_euclidean.append(row)
-        
+
         for row in ascii_euclidean:
             ascii_file_euclidean.write(''.join(row) + '\n')
         ascii_file_euclidean.close()
+
+'''
 
 test = Ascii('test.jpg', strings)
 test.grayscale()
 test.create_ascii_euclidean()
 
-
-
-
-
-
 test = Ascii('test.jpg',strings)
 test.grayscale()
 test.create_fontimage()
 test.create_ascii_simple()
+'''
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter.ttk import Combobox, Button
+import converter
 '''
 Author:Elmer Dema
 Solution to: Programming 2 by Prof. Dr. Markus Mayer
@@ -9,20 +11,21 @@ class Editor():
     def __init__(self, path, root):
         self.path = path
         self.root = root
+        self.selected_option = None  # Instance variable to store selected combobox value
 
     def header(self):
         label = tk.Label(self.root, text="ASCII Art Editor", font=("Times New Roman", 15))
         label.place(x=10, y=10)
 
         self.path_label = tk.Entry(self.root, width=50)
-        self.path_label.place(x=200, y=15)
+        self.path_label.place(x=160, y=15)
 
         select_label = tk.Button(self.root, text="Select Image", bg="lightgray", command=self.select_image)
-        select_label.place(x=350, y=10)
+        select_label.place(x=500, y=10)
 
         save_label = tk.Button(self.root, text="Save File", bg="lightgray")
-        save_label.place(x=440, y=10)
-    #only jpg and png files are allowed
+        save_label.place(x=580, y=10)
+
     def select_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg *.png")])
         if file_path:
@@ -30,27 +33,35 @@ class Editor():
             self.path_label.insert(tk.END, file_path)
 
     def body(self):
-        text_field = tk.Text(self.root, bg="lightgray", width=60, height=20)
-        text_field.place(x=10, y=50)
+        self.text_field = tk.Text(self.root, bg="lightgray", width=79, height=20)
+        self.text_field.place(x=10, y=50)
+
+    def insert_text_from_file(self, file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            self.text_field.delete(1.0, tk.END)
+            self.text_field.insert(tk.END, content)
 
     def footer(self):
-        font_size_entry = tk.Entry(self.root)
-        font_size_entry.place(x=10, y=400, width=25)
+        self.font_size_entry = tk.Entry(self.root)
+        self.font_size_entry.place(x=10, y=400, width=25)
 
-        char_width_entry = tk.Entry(self.root)
-        char_width_entry.place(x=60, y=400, width=25)
+        self.char_width_entry = tk.Entry(self.root)
+        self.char_width_entry.place(x=60, y=400, width=25)
 
-        char_height_entry = tk.Entry(self.root)
-        char_height_entry.place(x=110, y=400, width=25)
+        self.char_height_entry = tk.Entry(self.root)
+        self.char_height_entry.place(x=110, y=400, width=25)
 
-        char_height_entry = tk.Entry(self.root)
-        char_height_entry.place(x=110, y=400, width=25)
+        self.characters_entry = tk.Entry(self.root)
+        self.characters_entry.place(x=160, y=400, width=75)
 
-        characters_entry = tk.Entry(self.root)
-        characters_entry.place(x=160, y=400, width=75)
+        options = ["R", "G", "B"]
+        select_label = Combobox(self.root, values=options)
+        select_label.place(x=250, y=400, width=50)
+        select_label.bind("<<ComboboxSelected>>", self.update_selected_option)
 
-        generate_button = tk.Button(self.root, text="Generate", bg="lightgray")
-        generate_button.place(x=440, y=395)
+        generate_button = tk.Button(self.root, text="Generate", bg="lightgray", command=self.generate)
+        generate_button.place(x=580, y=395)
 
         font_size_label = tk.Label(self.root, text="Size", font=("Times New Roman", 10))
         font_size_label.place(x=8, y=420)
@@ -64,17 +75,39 @@ class Editor():
         characters_label = tk.Label(self.root, text="Characters", font=("Times New Roman", 10))
         characters_label.place(x=160, y=420)
 
+    def update_selected_option(self, event):
+        combobox = event.widget
+        self.selected_option = combobox.get()
 
-
+    def generate(self):
+        generator = converter.Ascii(
+            self.path_label.get(),
+            self.characters_entry.get(),
+            int(self.char_width_entry.get()),
+            int(self.char_height_entry.get()),
+            255,
+            int(self.font_size_entry.get()),
+            0,
+            self.selected_option,  # Use the stored selected option here
+        )
+        generator.grayscale()
+        generator.create_fontimage()
+        generator.create_ascii_simple()
+        generator.create_ascii_euclidean()
+        file_path = "ascii_euclidean.txt"
+        self.text_field.delete(1.0, tk.END)
+        self.insert_text_from_file(file_path)
 
 
 root = tk.Tk()
 root.title("ASCII Art Editor")
-root.geometry("500x450")
-test = Editor("test", root)
+root.geometry("650x450")
+test = Editor("test.jpg", root)
+
 
 test.header()
 test.body()
 test.footer()
+
 
 root.mainloop()
